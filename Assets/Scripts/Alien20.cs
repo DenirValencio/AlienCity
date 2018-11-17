191ae092cdffe4bfb8580937a860cfa00a9b85c9
Valencio <39912586+DenirValencio@users.noreply.github.com>
2018-11-17 15:09:20 -0200

    Add files via upload

---
+++ Assets/Scripts/Alien20.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alien20 : MonoBehaviour
+{
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("bullet"))
+        {
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }             
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
